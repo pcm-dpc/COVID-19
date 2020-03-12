9678e882daca32bee13884bf9c0b35daa8f7ffaa
fouche <tetrapharmakon@gmail.com>
2020-03-12 11:51:22 +0100

    py file to fetch data reg-by-reg

---
+++ fetch_reg.py
@@ -0,0 +1,40 @@
+import json
+
+regions = ["Abruzzo",
+           "Basilicata",
+           "Calabria",
+           "Campania",
+           "Emilia Romagna",
+           "Friuli Venezia Giulia",
+           "Lazio",
+           "Liguria",
+           "Lombardia",
+           "Marche",
+           "Molise",
+           "Piemonte",
+           "P.A. Bolzano",
+           "P.A. Trento",
+           "Puglia",
+           "Sardegna",
+           "Sicilia",
+           "Toscana",
+           "Umbria",
+           "Valle d'Aosta",
+           "Veneto"
+           ]
+
+
+def data_of(reg):
+    new = dict()
+    with open('dati-json/dpc-covid19-ita-regioni.json') as json_file:
+        data = json.load(json_file)
+        filtered = []
+        for thing in data:
+            if thing['denominazione_regione'] == reg:
+                filtered += [thing]
+        with open('dati-json-regxreg/' + str(reg).replace(" ", "") + '-data.json', 'w') as outfile:
+            json.dump(filtered, outfile)
+
+
+for rg in regions:
+    data_of(rg)
