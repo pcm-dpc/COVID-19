34f8c88691b22401a8aa6d90fd370ad997ea97f8
Stefano Miccoli <stefano.miccoli@polimi.it>
2020-11-12 00:22:16 +0100

    start checking invariants for dati-regioni

--- script/test_csv.py
+++ script/test_csv.py
@@ -1,24 +1,72 @@
 import sys
+import textwrap
 from pathlib import Path
 
 import pandas as pd
 
 
+def read_csv(p):
+    frame = pd.read_csv(
+        p,
+        encoding="UTF-8",
+        na_values=[""],
+        keep_default_na=False,
+        parse_dates=["data"],
+    )
+    return frame
+
+
+def _inv_regioni_0(regioni):
+    delta = regioni.totale_casi - (
+        regioni.totale_positivi + regioni.dimessi_guariti + regioni.deceduti
+    )
+    err = delta != 0
+    if err.any():
+        wrong = regioni.copy()[err][
+            [
+                "denominazione_regione",
+                "totale_positivi",
+                "dimessi_guariti",
+                "deceduti",
+                "totale_casi",
+            ]
+        ]
+        wrong["*DELTA*"] = delta
+        return wrong
+
+
+def check_regioni(pth, regioni):
+    errors = 0
+    wrong = _inv_regioni_0(regioni)
+    if wrong is not None:
+        print(
+            "* {}: INVARIANT ERROR 0".format(pth),
+            textwrap.indent(wrong.to_string(index=False), "  | "),
+            sep="\n",
+            file=sys.stderr,
+        )
+        errors += len(wrong)
+
+    return errors
+
+
 def main():
     errors = 0
-    try:
-        for i in sorted(Path(".").rglob("*.csv")):
-            try:
-                df = pd.read_csv(i)
-            except pd.errors.ParserError as exc:
-                print(
-                    "ParseError reading '{}': '{}'".format(i, str(exc).strip(),),
-                    file=sys.stderr,
-                )
-                errors += 1
-                continue
-    except Exception as exc:
-        sys.exit("Aborting: {}".format(exc))
+
+    # check dati-regioni
+    for pth in sorted(
+        Path("dati-regioni").glob("dpc-covid19-ita-regioni-????????.csv")
+    ):
+        try:
+            df = read_csv(pth)
+        except pd.errors.ParserError as exc:
+            print(
+                "ParseError reading '{}': '{}'".format(pth, str(exc).strip(),),
+                file=sys.stderr,
+            )
+            errors += 1
+            continue
+        errors += check_regioni(pth, df)
 
     if errors:
         sys.exit("Found {:d} error(s)".format(errors))
