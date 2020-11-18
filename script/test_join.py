1d5dd14d3305b69a617f5529bfb1bff60b99c34a
Stefano Miccoli <stefano.miccoli@polimi.it>
2020-11-18 19:03:00 +0100

    'test_join.py' refactored
    more consistent use of `csviter`, make code DRY

--- script/test_join.py
+++ script/test_join.py
@@ -3,40 +3,40 @@ import sys
 from pathlib import Path
 
 
-def csviter(pth, header):
+def csviter(pth):
     with open(pth, "r", newline="") as fp:
         r = csv.reader(fp)
-        if header != next(r):
-            raise ValueError("bad header")
-        for line in r:
-            yield line
+        for i, line in enumerate(r):
+            yield i, line
 
 
 def check(stem):
 
     folder = Path(f"dati-{stem}")
-    base = f"dpc-covid19-ita-{stem}{{}}.csv".format
+    base = f"dpc-covid19-ita-{stem}"
 
-    pth_all = folder / base("")
-    pth_single = sorted(folder.glob(base("-????????")))
+    pth_all = folder / (base + ".csv")
+    pth_single = sorted(folder.glob(base + "-????????.csv"))
 
     errors = 0
-    with open(pth_all) as fp:
-        csv_total = enumerate(csv.reader(fp), start=1)
-        i, header = next(csv_total)
+    r_all = csviter(pth_all)
+    i, header = next(r_all)
 
-        for pth in pth_single:
-            sub = csviter(pth, header)
-            for j, line in enumerate(sub, start=2):
-                i, line_t = next(csv_total)
-                if line_t != line:
-                    errors += 1
-                    print(
-                        "{} (record {:d}) != {} (record {:d})".format(
-                            pth_all, i, pth.stem, j
-                        ),
-                        file=sys.stderr,
-                    )
+    for pth in pth_single:
+        r_sub = csviter(pth)
+        _, head_sub = next(r_sub)
+        assert head_sub == header
+        for (j, line) in r_sub:
+            i, line_t = next(r_all)
+            if line_t != line:
+                errors += 1
+                print(
+                    "{} (record {:d}) != {} (record {:d})".format(
+                        pth_all, i, pth.stem, j
+                    ),
+                    file=sys.stderr,
+                )
+    assert len(list(r_all)) == 0
     return errors
 
 
